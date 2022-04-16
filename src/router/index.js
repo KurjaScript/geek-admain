10efe1b3cb036c2f52698a3e63d312da79496321
Kurja <Kurja0403@gmail.com>
2022-04-16 14:05:07 +0800

    项目规范

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import {
+    createRouter, //用于新建路由实例
+    createWebHashHistory, // 用于配置内部使用 hash 模式的路由，也就是 url 上会通过 # 来区分
+  } from 'vue-router'
+  import Home from '../pages/home.vue'
+  import About from '../pages/about.vue'
+  
+  const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+  
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+  
+  export default router
+  
