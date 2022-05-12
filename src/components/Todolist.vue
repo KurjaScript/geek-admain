aad763ab5de5a9ac6491dc4ca83036831461c77b
Kurja <Kurja0403@gmail.com>
2022-05-13 01:15:39 +0800

    将和清单相关的所有数据和方法，都放在useTodos函数内部

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1 @click="add">{{count}}</h1>  
+  </div>
+  <div>
     <input type="text" v-model="title" @keydown.enter="addTodo" />
     <button v-if="active < all" @click="clear">清理</button>
     <ul v-if="todos.length">
@@ -18,31 +21,41 @@
 
 <script setup>
 import { ref,computed } from "vue";
-let title = ref("");
-let todos = ref([{title:'学习Vue',done:false}])
+function useTodos() {
+  let title = ref("");
+  let todos = ref([{title:'学习Vue',done:false}])
 
-function addTodo() {
-  todos.value.push({
-    title: title.value,
-    done: false
-  });
-  title.value = "";
-}
-function clear() {
-  todos.value = todos.value.filter((v) => !v.done);
-}
-let active = computed(() => {
-  return todos.value.filter((v) => !v.done).length;
-});
-let all = computed(() => todos.value.length);
-let allDone = computed({
-  get: function () {
-    return active.value === 0;
-  },
-  set: function (value) {
-    todos.value.forEach((todo) => {
-      todo.done = value;
+  function addTodo() {
+    todos.value.push({
+      title: title.value,
+      done: false
     });
-  },
-});
+    title.value = "";
+  }
+  function clear() {
+    todos.value = todos.value.filter((v) => !v.done);
+  }
+  let active = computed(() => {
+    return todos.value.filter((v) => !v.done).length;
+  });
+  let all = computed(() => todos.value.length);
+  let allDone = computed({
+    get: function () {
+      return active.value === 0;
+    },
+    set: function (value) {
+      todos.value.forEach((todo) => {
+        todo.done = value;
+      });
+    },
+  });
+  return { title, todos, addTodo, clear, active, all, allDone }
+}
+
+let count = ref(1)
+function add() {
+  count.value++
+}
+let { title, todos, addTodo, clear, active, all, allDone } = useTodos()
+
 </script>
