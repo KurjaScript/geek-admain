e8367f90fe7b73f0ffe8bb7e740d0db19c4402c6
KurjaScript <Kurja0403@gmail.com>
2022-05-12 20:08:03 +0800

    将 Vue 2 的 Todolist 组件升级为 Vue 3

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,21 +1,48 @@
 <template>
   <div>
-    <h1 @click="add">{{count}}</h1>
+    <input type="text" v-model="title" @keydown.enter="addTodo" />
+    <button v-if="active < all" @click="clear">清理</button>
+    <ul v-if="todos.length">
+      <li v-for="todo in todos">
+        <input type="checkbox" v-model="todo.done" />
+        <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+      </li>
+    </ul>
+    <div v-else>暂无数据</div>
+    <div>
+      全选<input type="checkbox" v-model="allDone" />
+      <span> {{ active }} / {{ all }} </span>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-// 引入 ref 函数包裹数字，返回的 count 变量就是响应式的数据
-// 对于 ref 返回的响应式数据，需要修改 .value 才能生效
-let count = ref(1)
-function add(){
-    count.value++
-}
-</script>
+import { ref,computed } from "vue";
+let title = ref("");
+let todos = ref([{title:'学习Vue',done:false}])
 
-<style>
-h1 {
-  color: red;
+function addTodo() {
+  todos.value.push({
+    title: title.value,
+    done: false
+  });
+  title.value = "";
 }
-</style>
+function clear() {
+  todos.value = todos.value.filter((v) => !v.done);
+}
+let active = computed(() => {
+  return todos.value.filter((v) => !v.done).length;
+});
+let all = computed(() => todos.value.length);
+let allDone = computed({
+  get: function () {
+    return active.value === 0;
+  },
+  set: function (value) {
+    todos.value.forEach((todo) => {
+      todo.done = value;
+    });
+  },
+});
+</script>
