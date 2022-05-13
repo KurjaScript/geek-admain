c086685a651de0ddf20dabaeae6b698bdf0e56ab
KurjaScript <Kurja0403@gmail.com>
2022-05-13 16:25:00 +0800

    实现三颗星评分

---
+++ src/components/Rate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ rate }}
+    </div>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue';
+// 使用 defineProps 来规范传递数据的格式
+let props = defineProps({
+    value: Number
+})
+// 
+let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value))
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,12 @@
 <template>
     <h1>这是首页</h1>
     <Todolist />
+    <Rate :value="score"></Rate>
 </template>
 
 <script setup>
-import Todolist from '../components/Todolist.vue'
+import { ref } from 'vue';
+import Todolist from '../components/Todolist.vue';
+import Rate from '../components/Rate.vue';
+let score = ref(3)
 </script>
