a79f5c58f9bbf006a0e1ec6ddd2f8030d192b940
KurjaScript <Kurja0403@gmail.com>
2022-05-16 16:15:25 +0800

    实现列表过渡需求

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <h1 @click="add">{{count}}</h1>  
+    <h1 @click="add">{{ count }}</h1>
   </div>
   <div>
     <input type="text" v-model="title" @keydown.enter="addTodo" />
     <button v-if="active < all" @click="clear">清理</button>
     <ul v-if="todos.length">
-      <li v-for="todo in todos" :key="todo">
-        <input type="checkbox" v-model="todo.done" />
-        <span :class="{ done: todo.done }"> {{ todo.title }}</span>
-      </li>
+      <transition-group name="flip-list" tag="ul">
+        <li v-for="todo in todos" :key="todo.title">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+        </li>
+      </transition-group>
     </ul>
     <div v-else>暂无数据</div>
     <div>
@@ -19,14 +21,14 @@
   </div>
   <transition name="modal">
     <div class="info-wrapper" v-if="showModal">
-      <div class="info">请输入任务！</div>  
-    </div>
-  </transition>>
+      <div class="info">请输入任务！</div>
+    </div> </transition
+  >
 </template>
 
 <script setup>
-import { ref,computed, watchEffect } from "vue";
-import { useStorage } from '../utils/useStorage';
+import { ref, computed, watchEffect } from "vue";
+import { useStorage } from "../utils/useStorage";
 
 function useTodos() {
   let title = ref("");
@@ -37,19 +39,19 @@ function useTodos() {
   // watchEffect(() => {
   //   localStorage.setItem('todos', JSON.stringify(todos.value))
   // })
-  let todos = useStorage('todos',[])
-  let showModal = ref(false)
+  let todos = useStorage("todos", []);
+  let showModal = ref(false);
   function addTodo() {
-    if(!title.value) { 
-      showModal.value = true 
-      setTimeout(()=>{ 
-        showModal.value = false 
-      },1500) 
-      return 
+    if (!title.value) {
+      showModal.value = true;
+      setTimeout(() => {
+        showModal.value = false;
+      }, 1500);
+      return;
     }
     todos.value.push({
       title: title.value,
-      done: false
+      done: false,
     });
     title.value = "";
   }
@@ -70,39 +72,39 @@ function useTodos() {
       });
     },
   });
-  return { title, todos, addTodo, clear, active, all, allDone, showModal }
+  return { title, todos, addTodo, clear, active, all, allDone, showModal };
 }
 
-let count = ref(1)
-let color = ref('red')
+let count = ref(1);
+let color = ref("red");
 function add() {
-  count.value++
-  color.value = Math.random() > 0.5 ? "blue" : "red"
+  count.value++;
+  color.value = Math.random() > 0.5 ? "blue" : "red";
 }
-let { title, todos, addTodo, clear, active, all, allDone, showModal } = useTodos()
-
+let { title, todos, addTodo, clear, active, all, allDone, showModal } =
+  useTodos();
 </script>
 
 <style scoped>
 h1 {
-  color: v-bind(color)
+  color: v-bind(color);
 }
-.info-wrapper { 
-  position: fixed; 
-  top: 20px; 
-  width:200px;
+.info-wrapper {
+  position: fixed;
+  top: 20px;
+  width: 200px;
 }
-.info { 
-  padding: 20px; 
-  color: white; 
+.info {
+  padding: 20px;
+  color: white;
   background: #d88986;
 }
 .modal-enter-from {
   opacity: 0;
   transform: translateY(-60px);
 }
-.modal-enter-active { 
-  transition: all 0.3s ease; 
+.modal-enter-active {
+  transition: all 0.3s ease;
 }
 .modal-leave-to {
   opacity: 0;
@@ -110,5 +112,17 @@ h1 {
 }
 .modal-leave-active {
   transition: all 0.3s ease;
+}
+.flip-list-move {
+  transition: transform 0.8s ease;
+}
+.flip-list-enter-active,
+.flip-list-leave-active {
+  transition: all 1s ease;
+}
+.flip-list-enter-from,
+.flip-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
