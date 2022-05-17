4954416604ba9a5548f28d8ee18a76a66b0665ba
KurjaScript <Kurja0403@gmail.com>
2022-05-17 19:05:42 +0800

    粗糙的Header导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+      <el-menu
+        :default-active="1"
+        class="el-menu-demo"
+        mode="horizontal"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template v-slot:title>我的工作台</template>
+          <el-menu-item index="2-1">选项 1 </el-menu-item>
+          <el-menu-item index="2-2">选项 2 </el-menu-item>
+          <el-menu-item index="2-3">选项 3 </el-menu-item>
+          <el-submenu index="2-4">
+            <template v-slot:title>选项 4 </template>>
+            <el-menu-item index="2-4-1">选项 1 </el-menu-item>
+            <el-menu-item index="2-4-2">选项 2 </el-menu-item>
+            <el-menu-item index="2-4-3">选项 3 </el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4">
+          <a href="https://element3-ui.com" target="_blank">订单管理</a>
+        </el-menu-item>
+      </el-menu>
+    </el-header>
     <el-container>
       <el-aside width="200px">
         <div><router-link to="/">Home</router-link></div>
