19b377fbb21c4382e5caae09dcb4db6eb88a1412
KurjaScript <Kurja0403@gmail.com>
2022-05-17 16:11:47 +0800

    在项目的入口页面注册 Element3 内置的组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
 import router from './router/index'
 import store from './store'
 
@@ -8,4 +9,5 @@ const app = createApp(App)
 
 app.use(store)
     .use(router)
+    .use(Element3)
     .mount('#app')
