7b5b2db701123bf176591288f5ab217aaa41cef7
KurjaScript <Kurja0403@gmail.com>
2022-05-19 15:28:24 +0800

    引入ElementPlus

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Element3 from 'element3'
 import 'element3/lib/theme-chalk/index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router/index'
 import store from './store'
 
@@ -9,5 +11,5 @@ const app = createApp(App)
 
 app.use(store)
     .use(router)
-    .use(Element3)
+    .use(ElementPlus)
     .mount('#app')
