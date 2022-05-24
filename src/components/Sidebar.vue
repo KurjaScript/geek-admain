69b0108569a0f11fbdc54e41f56765dd2699c675
KurjaScript <Kurja0403@gmail.com>
2022-05-24 16:05:41 +0800

    router 注释

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="sidebar">
+    <!-- default-active: 默认激活菜单的 index -->
+    <!-- router: 是否启用 vue-router 模式。启用该模式会在激活导航时以 index 作为 path 进行路由跳转 -->
     <el-menu
       class="sidebar-el-menu"
       :default-active="onRoutes"
