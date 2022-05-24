e20ea61d4a1a31e2108158adfb637f1a88af6d72
KurjaScript <Kurja0403@gmail.com>
2022-05-24 15:27:54 +0800

    Tags 组件

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state () {
-    return {
+  state: {
       count: 666,
       collapse: false,
       tagsList: [],
-      
-    }
   },
 
   getters: {
@@ -22,7 +19,17 @@ export default createStore({
     },
     handleCollapse(state, data) {
       state.collapse = data
-    }
+    },
+    delTagsItem(state, data) {
+      state.tagsList.splice(data.index, 1)
+    },
+    setTagsItem(state, data) {
+      state.tagsList.push(data)
+    },
+    closeTagsOther(state, data) {
+      state.tagsList = data
+    },
+
   },
 
   actions: {
