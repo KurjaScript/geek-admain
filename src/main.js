6b87c60dcbecbd7506fbb9cfabd25d8941aab7d3
KurjaScript <Kurja0403@gmail.com>
2022-05-25 10:38:52 +0800

    重新安装 axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
 const app = createApp(App)
@@ -10,3 +11,4 @@ app
     .use(store)
     .use(router)
     .mount('#app')
+    .prototype.$http = axios
